5c007ca78894720ee9132d5979ae00087cf4eca5
Paul Fasola <paul.fasola@gmail.com>
2018-11-04 22:19:52 +0100

    Update english and add french locales

--- locales/en.js
+++ locales/en.js
@@ -1,16 +1,29 @@
 module.exports = {
     translation: {
-        SKILL_NAME: "Help me buy my Lambo",
-        HELLO: ["Sure! You can also indicate me the model of lambo you want"],
+        SKILL_NAME: "help me buy my Lambo",
+        HELLO: [
+            "Sure! In which crypto-currency do you want the amount ? You can also indicate me the model of the lambo!",
+        ],
         HELP: [],
         FALLBACK_REPROMPT: [
             "This skill can't help you with that. It can help you learn about Lambo and fancy crypto stuff. What can I help you with?",
         ],
         FALLBACK_MESSAGE: ["What can I help you with?"],
-        RES_CRYPTO_QT: [
+        RES_CRYPTO_QT_WITH_MODEL: [
             "At this time, you need %s %s if you want to buy your Lambo %s.",
             "For %s %s, you can buy your Lambo' %s right now !",
             "%s %s are actually necessary if you want this Lamborghini %s.",
+            "You need %s %s",
+        ],
+        RES_CRYPTO_QT_NO_DATA: [
+            "Hum, I dont have any information regarding this model. For a classic lambo, you will need to spend %s %s.",
+            "I can't find this model in my records. For your information, you need approximately %s %s for a classic one.",
+        ],
+        RES_CRYPTO_QT_NO_MODEL: [
+            "At this time, you need %s %s if you want to buy a Lambo.",
+            "For %s %s, you can buy your Lambo' right now !",
+            "%s %s are actually necessary if you want it.",
+            "You need %s %s",
         ],
         ERROR: [
             "Sorry, I can't understand the command. Please say again.",
--- locales/fr.js
+++ locales/fr.js
@@ -1,11 +1,33 @@
 module.exports = {
     translation: {
-        SKILL_NAME: "",
-        HELLO: [""],
-        HELP: [""],
-        FALLBACK_REPROMPT: [""],
-        FALLBACK_MESSAGE: [""],
-        RES_CRYPTO_QT: [""],
-        ERROR: [""],
+        SKILL_NAME: "Aide moi a acheter ma lambo",
+        HELLO: [
+            "Bien-sûr, précisez la crypto-monnaie et  le modèle de Lamborghini si vous en souhaitez un en particulier. Je vous donnerai la quantité nécessaire actuellement.",
+        ],
+        HELP: [],
+        FALLBACK_REPROMPT: [
+            "Cette skill ne peut pas vous aider pour cela. Elle peut vous informer sur le coût d'une lambo en temps réel. Comment puis-je donc vous aider ?",
+        ],
+        FALLBACK_MESSAGE: ["What can I help you with?"],
+        RES_CRYPTO_QT_WITH_MODEL: [
+            "Actuellement, il faudra %s %s si vous voulez votre Lambo %s.",
+            "Pour %s %s, vous pouvez acheter votre Lambo' %s dés maintenant !",
+            "%s %s sont nécéssaires si vous souhaitez cette Lamborghini %s.",
+            "Il vous faut %s %s",
+        ],
+        RES_CRYPTO_QT_NO_DATA: [
+            "Hum, je n'ai pas d'informations concernant ce modèle. Pour une lambo standard cependant, vous devez dépenser %s %s.",
+            "Je n'arrive pas à trouver ce modèle dans mes enregistrements. Cependant, pour information, il vous faudra plus ou moins %s %s pour une lambo standard.",
+        ],
+        RES_CRYPTO_QT_NO_MODEL: [
+            "Actuellement, il vous faut %s %s si vous la voulez.",
+            "Pour %s %s, vous pouvez commander votre lambo tout de suite !",
+            "%s %s sont nécéssaires si vous en voulez une.",
+            "Il vous faut %s %s",
+        ],
+        ERROR: [
+            "Désolé, je n'ai pas compris la commande. Veuillez répéter.",
+            "Oups, je n'ai pas compris, pouvez-vous répéter ? ?",
+        ],
     },
 };
